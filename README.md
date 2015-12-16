1780b0624fe5694b3df6f3eea9b4bfb371cc1fbd
Alberto Buffolino <a.buffolino@gmail.com>
2015-12-16 22:23:30 +0100

    First case: Splitter Regex.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A collection of brief codes, quite useless or unlikely applicable, to solve in a shorter or involved manner some relatively simple problems.
 
 Problems that, for majority of cases, come from my friend and colleague Emmanuel Schoysman, and which he uses to torture me for days and days. :)
+
+## Splitter Regex ##
+A exercise/challenge to build a (monstrous) regex to split a string, in all possible cases.
+[Explanation](splitterRegex.txt) and [Python code.](splitterRegex.py)
---
+++ splitterRegex.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# A Python proof of concepts about splitting with a regex.
+# See splitterRegex.txt for more info.
+import re
+
+def splitterRegex(input, subExp):
+	ourMagic = r'(?:{subExp})|(.*?)(?={subExp})|(.+)(?!{subExp})'
+	exp = re.compile(ourMagic.format(subExp=subExp))
+	validGroups = []
+	for match in exp.finditer(input):
+		for group in match.groups():
+			# we want only non-None groups
+			if group:
+				validGroups.append(group)
+	return validGroups
+
+# some tests
+if __name__ == "__main__":
+	from collections import OrderedDict
+	# a examples dict with (subExp,input) as (key,value) pairs
+	d = OrderedDict()
+	# at start, at end, char
+	d["a"] = "abba"
+	# middle, single, char
+	d["b"] = "abcabcabcd"
+	# middle, double, char
+	d["c"] = "accbccd"
+	# middle, single word
+	d["singleword"] = "Oh, poor singleword, dead in our experiments..."
+	# middle, repeated word (without spaces)
+	d["doubleword"] = "I mean doubleworddoubleword, obviously."
+	# 3-digits regex, with a postal tracking code
+	d[r"\d{3}"] = "KZ717632487IT"
+	for (subExp,input) in d.items():
+		print "Input: %s"%input
+		print "SubExp: %s"%subExp
+		res = splitterRegex(input, subExp)
+		print "List of results: %s"%res
